8e748060a9addb30de2125dc78ddf7974df8ccf8
Tulips01 <njerigladys96@gmail.com>
2022-03-13 09:53:32 -0700

    guuy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the phrase 
- *
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
  * Return: Always 0 (Success)
  */
 int main(void)
