d2d35281dc74549e49ac759fbe43c8c8e5d0b429
Tulips01 <njerigladys96@gmail.com>
2022-03-13 09:36:49 -0700

    guuy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - prints "Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
- * */
+ * 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
